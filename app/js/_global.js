bbe1d2021b7034d3b3f319c39d589d3a5051b489
Anas ali <anas.cd.97@gmail.com>
2022-03-16 20:52:06 +0300

    finished HTML&SCSS

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,14 +1,37 @@
-  
+const id = document.querySelector('#adid');  // advice id
+const ad = document.querySelector('#advice');// advice 
+const btn = document.querySelector('#btndicer'); // dice btn
+let slipdata;
+
 // Global
 app.global = {
     init: function(){ // Load all global functions here
         console.log("load global functions");
-        app.global.loadHeader();
+        app.global.loadAdvice();
     },
-    loadHeader: function(){ // Some specific function
-        console.log("loadHeader()");
-    }
+    loadAdvice: function(){ // Some specific function
+        console.log("loadAdvice()");
+
+        // calling api
+        $.getJSON('https://api.adviceslip.com/advice', function(data) {
+            
+            console.log("getJSON intered");
+            slipdata =  data.slip; // assigning the object to slipdata
+            console.log(slipdata);          //
+            console.log(slipdata.id);       // checking 
+            console.log(slipdata.advice);   //
+
+            id.innerHTML=slipdata.id;       // writing id
+            ad.innerHTML=slipdata.advice;   // writing advice
+            
+        });
+    }   
 }
 
 // Run the global stuff
 app.global.init();
+
+btn.addEventListener('click', () => {
+    console.log("click preformed"); 
+    app.global.loadAdvice();
+});
